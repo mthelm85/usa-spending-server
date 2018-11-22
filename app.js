745f74ed4896163538695a689d34c6ecce8445e5
Matt Helm <mthelm85@gmail.com>
2018-11-22 13:34:34 -0600

    merged app and getData

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const MongoClient = require('mongodb').MongoClient,
-      exec = require('child_process').exec
+      exec = require('child_process').exec,
+      getData = require('./getData.js')
 
 const url = 'mongodb://localhost:27017/usa-spending'
 
-MongoClient.connect(url, { useNewUrlParser: true }, async (err, client) => {
-  if (err) throw err
-  const db = client.db('usa-spending'),
-        res = await db.collection('awards').deleteMany({})
-  console.log(`Deleted ${res.deletedCount} documents`)
-  client.close(() => {
-    const cmd = 'mongoimport -d usa-spending -c awards --type csv data.csv --headerline'
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) throw err
-      console.log(stdout)
-      console.log(stderr)
-    })
+const updateMongoDB = async () => {
+  await getData.updateData()
+  MongoClient.connect(url, { useNewUrlParser: true }, async (err, client) => {
+    if (err) throw err
+    const db = client.db('usa-spending'),
+          res = await db.collection('awards').deleteMany({})
+    console.log(`Deleted ${res.deletedCount} documents`)
+    await client.close()
   })
-})
+  const cmd = 'mongoimport -d usa-spending -c awards --type csv all_contracts_prime_transactions_1.csv --headerline'
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) throw err
+    console.log(stdout)
+    console.log(stderr)
+  })
+}
 
-
-// Better performance to simply delete all docs in the collection and then import again
-// use MongoClient to call db.collection.drop(), then above code to import
+updateMongoDB()
