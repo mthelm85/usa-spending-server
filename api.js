e19af0ac286d69779958873604d1a2e2bc32bb99
Matt Helm <mthelm85@gmail.com>
2018-11-25 11:22:30 -0600

    edited cors

--- api.js
+++ api.js
@@ -8,16 +8,22 @@ const bodyParser = require('body-parser'),
       morgan = require('morgan'),
       port = process.env.PORT || 3000,
       url = 'mongodb://localhost:27017/usa-spending',
-      schedule = require('node-schedule')
+      schedule = require('node-schedule'),
+      whitelist = ['http://localhost:8080']
 
-const app = express()
+const app = express(),
+      corsOptions = {
+        origin (origin, callback) {
+          if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+          } else {
+            callback(new Error('Not allowed by CORS'))
+          }
+        }
+      }
 
 app.use(morgan('dev'))
-app.use(cors({
-  origin: ['*'],
-  methods: ['GET'],
-  credentials: true
-}))
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
@@ -35,11 +41,9 @@ schedule.scheduleJob(updateRule, () => {
   require('./updateMongo.js')(exec, getData, MongoClient, url)
 })
 
-// schedule.scheduleJob(deleteRule, () => {
-//   require('./deleteNoLaborStandards.js')(MongoClient, url)
-// })
-
-require('./deleteNoLaborStandards.js')(MongoClient, url)
+schedule.scheduleJob(deleteRule, () => {
+  require('./deleteNoLaborStandards.js')(MongoClient, url)
+})
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
   if (err) throw err
